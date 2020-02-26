0822b79bb90dbdac1d6a3a573b4d7798d40396fd
Johanna <svallingson@gmail.com>
2020-02-26 14:32:39 +0100

    first commit

---
+++ src/main/java/se/iths/bigjohanna/myservice/UserController.java
@@ -0,0 +1,10 @@
+package se.iths.bigjohanna.myservice;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+
+
+}
---
+++ src/test/java/se/iths/bigjohanna/myservice/MyserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.bigjohanna.myservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
